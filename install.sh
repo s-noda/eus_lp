584cae26cc0ce7ddaebfb9358e8c0152d92e3a80
s-noda <s-noda@jsk.t.u-tokyo.ac.jp>
2016-01-30 20:42:00 +0900

    apt installing lp libs

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+function check_libs(){
+    echo -e "\e[33minstall libs ... \e[m";
+    if [ ! -d "/usr/include/lpsolve" ];
+    then
+	echo -e "\e[33mlpsolve installing ... \e[m";
+	sudo aptitude install lp-solve;
+    fi
+    if [ ! -d "/usr/lib/lp_solve" ];
+    then
+	echo -e "\e[33mlpsolve_lib installing ... \e[m";
+	sudo aptitude install liblpsolve55-dev;
+    fi
+    if [ ! -d "/usr/lib/mumps" ];
+    then
+	echo -e "\e[33mmumps_lib installing ... \e[m";
+	sudo aptitude install libmumps-dev;
+    fi
+    echo -e "\e[33mdone\e[m";
+}
+
+check_libs;
