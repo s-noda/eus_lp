093799f896aad3688037e70c2c345f815c116196
s-noda <s-noda@jsk.t.u-tokyo.ac.jp>
2016-01-30 23:04:56 +0900

    install clp from apt

--- install.sh
+++ install.sh
@@ -12,11 +12,16 @@ function check_libs(){
 	echo -e "\e[33mlpsolve_lib installing ... \e[m";
 	sudo aptitude install liblpsolve55-dev;
     fi
-    if [ ! -d "/usr/lib/mumps" ];
+    if [ ! -d "/usr/include/coin" ];
     then
-	echo -e "\e[33mmumps_lib installing ... \e[m";
-	sudo aptitude install libmumps-dev;
+	echo -e "\e[33mclp installing ... \e[m";
+	sudo aptitude install coinor-libclp-dev
     fi
+    # if [ ! -d "/usr/lib/mumps" ];
+    # then
+    # 	echo -e "\e[33mmumps_lib installing ... \e[m";
+    # 	sudo aptitude install libmumps-dev;
+    # fi
     echo -e "\e[33mdone\e[m";
 }
 
